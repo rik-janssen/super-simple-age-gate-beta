94dd8198d1712597cff0128fd6101b4b63aadd8f
TBC <shoutatrikjanssen@gmail.com>
2019-11-08 13:22:35 +0100

    1.2

--- simple-age-gate.php
+++ simple-age-gate.php
@@ -3,7 +3,7 @@
 * Plugin Name: Super Simple Age Gate | Beta
 * Plugin URI: https://betacore.tech/super-simple-age-gate-for-wordpress/
 * Description:  It's all about keeping the youngsters out of your sites while enabling crawlers to know what is on your pages.
-* Version: 1.1
+* Version: 1.2
 * Author: Beta
 * Author URI: https://betacore.tech/
 * Text Domain: betagate
